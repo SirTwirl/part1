dd2be15b1c86cce85aff822820775384535f7a36
SirTwirl <j.kruk123@o2.pl>
2026-09-04 12:48:46 +0200

    added Total component

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -16,6 +16,12 @@ const Content = (props) => {
   )
 }
 
+const Total = (props) => {
+  <>
+    <p>Number of exercises {props.sumofexercises}</p>
+  </>
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -29,7 +35,7 @@ const App = () => {
     <div>
       <Header course={course}/>
       <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Total sumofexercises={exercises1 + exercises2 + exercises3}/>
     </div>
   )
 }
