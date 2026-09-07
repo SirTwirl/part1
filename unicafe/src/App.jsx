2de9a1be3338e99404f7bdc10a2ee3791ea199c1
SirTwirl <j.kruk123@o2.pl>
2026-09-07 14:06:29 +0200

    feat: add event handlers for feedback buttons

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -8,19 +8,25 @@ const Button = (props) => {
   )
 }
 
+const Statistic = (props) => {
+  return (
+    <p>{props.text} {props.value}</p>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   const handleGoodButton = () => {
-
+    setGood(good + 1)
   }
   const handleNeutralButton = () => {
-    
+    setNeutral(neutral + 1)
   }
   const handleBadButton = () => {
-    
+    setBad(bad + 1)
   }
 
   return (
@@ -30,6 +36,9 @@ const App = () => {
       <Button onClick={handleNeutralButton} text='neutral'/>
       <Button onClick={handleBadButton} text='bad'/>
       <h1>statistic</h1>
+      <Statistic text='good' value={good}/>
+      <Statistic text='neutral' value={neutral}/>
+      <Statistic text='bad' value={bad}/>
     </div>
   )
 }
