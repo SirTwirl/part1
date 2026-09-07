c5fa3fd17b9ea465ae21411739bef984ff66d7f6
SirTwirl <j.kruk123@o2.pl>
2026-09-07 14:02:24 +0200

    feat: add feedback buttons

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,13 +1,35 @@
 import {useState} from 'react'
 
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+    {props.text}
+    </button>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodButton = () => {
+
+  }
+  const handleNeutralButton = () => {
+    
+  }
+  const handleBadButton = () => {
+    
+  }
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <Button onClick={handleGoodButton} text='good'/>
+      <Button onClick={handleNeutralButton} text='neutral'/>
+      <Button onClick={handleBadButton} text='bad'/>
+      <h1>statistic</h1>
     </div>
   )
 }
